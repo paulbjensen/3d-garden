05880a076b09917636d9e913b63009b56c4c6d8a
Paul Jensen <paulbjensen@gmail.com>
2025-04-28 11:34:04 +0200

    Added Sentry support

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
+import * as Sentry from "@sentry/svelte";
+// Initialize the Sentry SDK here
+Sentry.init({
+  dsn: "https://10f35c3bdbe74919bdd8dbfe0e9a5b8e@o95413.ingest.us.sentry.io/208704",
+  // Adds request headers and IP for users, for more info visit:
+  // https://docs.sentry.io/platforms/javascript/guides/svelte/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+  integrations: [
+  ],
+});
 
 const app = mount(App, {
   target: document.getElementById('app')!,
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), sentryVitePlugin({
+    org: "anephenix",
+    project: "3d-garden"
+  })],
+
+  build: {
+    sourcemap: true
+  }
 })
