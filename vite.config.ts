8e2a6dcb1339037a4d31e452a3b3e6e3a9d3da6b
Paul Jensen <paulbjensen@gmail.com>
2026-05-01 23:52:13 +0200

    Switched to Cloudflare pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { cloudflare } from "@cloudflare/vite-plugin";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [
+		cloudflare(),
 		svelte(),
 		sentryVitePlugin({
 			org: "anephenix",
